d755296b56b4eec0932c0ca4a3cccc7b0fd9c64d
afeef <janjua.afeef@gmail.com>
2024-01-16 23:42:40 +0500

    removed shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import Image from 'next/image'
-import { Button } from "@/components/ui/button"
-
 export default function Home() {
   return (
     <main>
         <div>
-            <Button>Click me</Button>
+            <div>Click me</div>
         </div>
     </main>
   )
